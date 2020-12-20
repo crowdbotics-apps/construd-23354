a43ca59fcdc38dc0614f2314041255a5742fc042
Crowdbotics <team@crowdbotics.com>
2020-12-20 22:44:38 +0000

    Update components

--- src/features/BlankScreen0184991/screens/blank.js
+++ src/features/BlankScreen0184991/screens/blank.js
@@ -27,37 +27,79 @@ export default class Blank extends React.Component {
   state = { TextInput_7: "", TextInput_11: "" }
 
   render = () => (
-    <View>
+    <View style={styles.View_1}>
       <Image
         resizeMode="contain"
         source={{
           uri:
             "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/logo_yAoeSYS.png"
         }}
+        style={styles.Image_3}
       />
-      <Text>Login</Text>
+      <Image
+        resizeMode="stretch"
+        source={{
+          uri:
+            "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/barra.PNG"
+        }}
+        style={styles.Image_31}
+      />
+      <Text style={styles.Text_5}>Login</Text>
       <TextInput
         placeholder=""
+        style={styles.TextInput_7}
         value={this.state.TextInput_7}
         onChangeText={nextValue => this.setState({ TextInput_7: nextValue })}
       />
-      <Text>Contraseña</Text>
+      <Text style={styles.Text_9}>Contraseña</Text>
       <TextInput
         placeholder="*****"
+        style={styles.TextInput_11}
         value={this.state.TextInput_11}
         onChangeText={nextValue => this.setState({ TextInput_11: nextValue })}
       />
-      <Button title="Ingresar" onPress={() => alert("Pressed!")} />
+      <Button
+        title="Ingresar"
+        color="#f6a46d"
+        style={styles.Button_13}
+        onPress={() => alert("Pressed!")}
+      />
+      <Text style={styles.Text_15}>Versión 1.0</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  View_1: {},
-  Image_3: {},
-  Text_5: {},
-  TextInput_7: {},
-  Text_9: {},
-  TextInput_11: {},
-  Button_13: {}
+  View_1: {
+    color: "#000",
+    backgroundColor: "#ec6307",
+    fontFamily: "Roboto-Regular"
+  },
+  Image_3: { alignSelf: "center" },
+  Image_31: { height: 44, alignSelf: "center" },
+  Text_5: {
+    color: "#fff",
+    backgroundColor: "#ec6307",
+    fontFamily: "Roboto-Regular"
+  },
+  TextInput_7: { width: "90%", alignSelf: "center" },
+  Text_9: {
+    color: "#fff",
+    backgroundColor: "#ec6307",
+    fontFamily: "Roboto-Regular"
+  },
+  TextInput_11: { width: "90%", alignSelf: "center" },
+  Button_13: {
+    width: "95%",
+    marginLeft: 7,
+    alignSelf: "center",
+    color: "#fff",
+    fontFamily: "Roboto-Regular"
+  },
+  Text_15: {
+    alignSelf: "center",
+    color: "#fff",
+    backgroundColor: "#ec6307",
+    fontFamily: "Roboto-Regular"
+  }
 })
